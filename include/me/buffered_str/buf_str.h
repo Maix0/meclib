fc297193ac0ad5f0bd3eb5dac7db5cfb2d51aadb
Maix0 <maix522@gmail.com>
2023-12-26 22:43:49 +0100

    updated buf_str

--- include/me/buffered_str/buf_str.h
+++ include/me/buffered_str/buf_str.h
@@ -6,21 +6,24 @@
 /*   By: maiboyer <maiboyer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 17:54:28 by maiboyer          #+#    #+#             */
-/*   Updated: 2023/11/22 12:39:42 by maiboyer         ###   ########.fr       */
+/*   Updated: 2023/12/26 19:54:23 by maiboyer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUF_STR_H
-# define BUF_STR_H
-# include "me/types.h"
+#define BUF_STR_H
+#include "me/types.h"
 
 typedef struct s_buffer_str
 {
-	t_str		buf;
-	t_usize		capacity;
-	t_usize		len;
-}				t_buffer_str;
-bool			push_str_buffer(t_buffer_str *buf, t_const_str to_push);
-t_buffer_str	alloc_new_buffer(t_usize capacity);
+	t_str	buf;
+	t_usize capacity;
+	t_usize len;
+} t_buffer_str;
+
+bool		 push_str_buffer(t_buffer_str *buf, t_const_str to_push);
+bool		 push_str_char(t_buffer_str *buf, char to_push);
+void		 str_clear(t_buffer_str *buf);
+t_buffer_str alloc_new_buffer(t_usize capacity);
 
 #endif
--- src/buffered_str/mod.c
+++ src/buffered_str/mod.c
@@ -6,23 +6,24 @@
 /*   By: maiboyer <maiboyer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 17:52:12 by maiboyer          #+#    #+#             */
-/*   Updated: 2023/12/11 18:43:31 by maiboyer         ###   ########.fr       */
+/*   Updated: 2023/12/26 19:54:07 by maiboyer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "me/buffered_str/buf_str.h"
 #include "me/mem/mem_alloc.h"
+#include "me/mem/mem_set_zero.h"
 #include "me/string/str_l_cat.h"
 #include "me/string/str_l_copy.h"
 #include "me/string/str_len.h"
 #include "me/types.h"
 #include <stdlib.h>
 
-bool	push_str_buffer(t_buffer_str *buf, t_const_str to_push)
+bool push_str_buffer(t_buffer_str *buf, t_const_str to_push)
 {
-	t_usize	to_push_len;
+	t_usize to_push_len;
 	t_str	temp_buffer;
-	t_usize	new_capacity;
+	t_usize new_capacity;
 
 	if (buf == NULL || to_push == NULL)
 		return (true);
@@ -43,11 +44,30 @@ bool	push_str_buffer(t_buffer_str *buf, t_const_str to_push)
 	return (false);
 }
 
-t_buffer_str	alloc_new_buffer(t_usize capacity)
+bool push_str_char(t_buffer_str *buf, char to_push)
 {
-	t_buffer_str	out;
-	t_str			buf;
+	char push_str[2];
 
+	push_str[0] = to_push;
+	push_str[1] = 0;
+
+	return (push_str_buffer(buf, push_str));
+}
+
+void str_clear(t_buffer_str *buf)
+{
+	mem_set_zero(buf->buf, buf->capacity);
+	buf->len = 0;
+	return;
+}
+
+t_buffer_str alloc_new_buffer(t_usize capacity)
+{
+	t_buffer_str out;
+	t_str		 buf;
+
+	if (capacity == 0)
+		capacity = 16;
 	buf = mem_alloc(sizeof(char) * capacity);
 	if (buf == NULL)
 	{
