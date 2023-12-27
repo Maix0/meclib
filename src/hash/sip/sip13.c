17858f3cca4ca325a6e6cfb8b96bfb7183900059
Maix0 <maix522@gmail.com>
2023-12-27 18:19:57 +0100

    fixed hashmap

--- src/hash/sip/sip13.c
+++ src/hash/sip/sip13.c
@@ -6,7 +6,7 @@
 /*   By: maiboyer <maiboyer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/10 19:32:28 by maiboyer          #+#    #+#             */
-/*   Updated: 2023/12/11 15:38:13 by maiboyer         ###   ########.fr       */
+/*   Updated: 2023/12/27 16:48:13 by maiboyer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,11 @@ t_hasher	hasher_sip13_new(void)
 
 	inner = mem_alloc(sizeof(t_sip13));
 	inner->state = create_state_with_key(0, 0);
+	inner->k0 = 0;
+	inner->k1 = 0;
 	out.hasher = inner;
 	out.hash_bytes = (t_hash_bytes)sip13_write_bytes;
-	out.finish = (t_hash_finish)sip13_finish;
+	out.finish = (t_hasher_finish)sip13_finish;
+	out.reset_and_finish = (t_hasher_reset_and_finish)sip13_reset_and_finish;
 	return (out);
 }
--- src/hash/sip/sip_utils2.c
+++ src/hash/sip/sip_utils2.c
@@ -6,14 +6,14 @@
 /*   By: maiboyer <maiboyer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 14:29:03 by maiboyer          #+#    #+#             */
-/*   Updated: 2023/12/11 14:56:37 by maiboyer         ###   ########.fr       */
+/*   Updated: 2023/12/27 16:52:17 by maiboyer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "me/hash/sip/sip_utils.h"
 #include "me/num/usize.h"
 
-void	compress(t_sip_state *state)
+void compress(t_sip_state *state)
 {
 	state->v0 = state->v0 + state->v1;
 	state->v1 = usize_rotate_left(state->v1, 13);
@@ -31,9 +31,9 @@ void	compress(t_sip_state *state)
 	state->v2 = usize_rotate_left(state->v2, 32);
 }
 
-t_sip_state	create_state_with_key(t_u64 k0, t_u64 k1)
+t_sip_state create_state_with_key(t_u64 k0, t_u64 k1)
 {
-	t_sip_state	state;
+	t_sip_state state;
 
 	state = (t_sip_state){.v0 = 0, .v1 = 0, .v2 = 0, .v3 = 0};
 	state.v0 = k0 ^ 0x736f6d6570736575;
@@ -41,4 +41,29 @@ t_sip_state	create_state_with_key(t_u64 k0, t_u64 k1)
 	state.v2 = k0 ^ 0x6c7967656e657261;
 	state.v3 = k1 ^ 0x7465646279746573;
 	return (state);
+}
+
+t_u64 sip13_reset_and_finish(t_sip13 *self)
+{
+	t_sip_state state;
+	t_u64		b;
+	t_u64		ret;
+
+	state = self->state;
+	b = (((t_u64)self->length & 0xff) << 56) | self->tail;
+	state.v3 ^= b;
+	compress(&state);
+	state.v0 ^= b;
+	state.v2 ^= 0xff;
+	compress(&state);
+	compress(&state);
+	ret = (state.v0 ^ state.v1 ^ state.v2 ^ state.v3);
+	self->length = 0;
+	self->state.v0 = self->k0 ^ 0x736f6d6570736575;
+	self->state.v1 = self->k1 ^ 0x646f72616e646f6d;
+	self->state.v2 = self->k0 ^ 0x6c7967656e657261;
+	self->state.v3 = self->k1 ^ 0x7465646279746573;
+	self->ntail = 0;
+
+	return (ret);
 }
